7247668a39d9fc9dcece174ffd14407492a9d490
BhavadeepGunna <gunnabhavadeep@gmail.com>
2024-07-10 20:17:21 +0530

    Initial commit

---
+++ server/middleware/authtoken.js
@@ -0,0 +1,37 @@
+const jwt=require('jsonwebtoken')
+
+
+const authtoken=async(req,res,next)=>{
+    try{
+        const token=req.cookies?.token
+
+
+
+        if(!token){
+            return res.status(200).json({
+                message:"user not login",
+                error:true,
+                success:false
+
+            })
+        }
+        jwt.verify(token, process.env.TOKEN_SECRET_KEY, function(err, decoded) {
+
+            req.userid=decoded?._id
+            next();
+
+          });
+
+        
+        
+    }catch(err){
+        res.status(400).json({
+            message:err.message||err,
+            data:[],
+            error:true,
+            success:false
+        })
+
+    }
+}
+module.exports=authtoken
